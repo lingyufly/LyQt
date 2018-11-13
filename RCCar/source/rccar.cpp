6da2855409ccf9c1d18c5ee2ddbe75ce0b7a278b
ly <lingyuwhu@outlook.com>
2018-11-13 11:22:21 +0800

    * ui designed

--- RCCar/source/rccar.cpp
+++ RCCar/source/rccar.cpp
@@ -15,6 +15,7 @@
 #include <QBitMap>
 #include <QJsonDocument>
 #include <QJsonObject>
+#include <QButtonGroup>
 
 ControlWidget::ControlWidget(QWidget *parent /*= NULL*/, Qt::WindowFlags fl/*= Qt::windowFlags()*/)
 {
@@ -34,22 +35,40 @@ void ControlWidget::setupUi()
     m_picLabel = new QLabel(this);
     m_mainLayout->addWidget(m_picLabel);
 
+    QPixmap pixmap;
     m_leftBtn = new QPushButton(this);
     m_leftBtn->setCheckable(true);
     m_leftBtn->setFlat(true);
+    pixmap = QPixmap(":/image/turnleft.png");
+    m_leftBtn->setIcon(pixmap);
     m_leftBtn->show();
+
     m_backBtn = new QPushButton(this);
     m_backBtn->setCheckable(true);
     m_backBtn->setFlat(true);
+    pixmap = QPixmap(":/image/goback.png");
+    m_backBtn->setIcon(pixmap);
     m_backBtn->show();
+
     m_rightBtn = new QPushButton(this);
     m_rightBtn->setCheckable(true);
     m_rightBtn->setFlat(true);
+    pixmap = QPixmap(":/image/turnright.png");
+    m_rightBtn->setIcon(pixmap);
     m_rightBtn->show();
+
     m_foreBtn = new QPushButton(this);
     m_foreBtn->setCheckable(true);
     m_foreBtn->setFlat(true);
-    m_foreBtn->show(); 
+    pixmap = QPixmap(":/image/gofore.png");
+    m_foreBtn->setIcon(pixmap);
+    m_foreBtn->show();
+
+    m_drctBtnGroup = new QButtonGroup(this);
+    m_drctBtnGroup->addButton(m_foreBtn);
+    m_drctBtnGroup->addButton(m_backBtn);
+    m_drctBtnGroup->addButton(m_leftBtn);
+    m_drctBtnGroup->addButton(m_rightBtn);
 
     m_viewSlider = new QSlider(this);
     m_viewSlider->show();
@@ -57,32 +76,31 @@ void ControlWidget::setupUi()
     m_viewSlider->setMinimum(0);
     m_viewSlider->setMaximum(180);
     m_viewSlider->setValue(90);
+    m_resetViewBtn = new QPushButton(this);
 
     m_settingBtn = new QPushButton(this);
     m_settingBtn->setFlat(true);
+    pixmap = QPixmap(":/image/setting.png");
+    m_settingBtn->setIcon(pixmap);
     m_settingBtn->show();
 
     m_infoLabel = new QLabel(this);
-    //m_infoLabel->setFrameShape(QFrame::Box);
     m_infoLabel->show();
 
     m_logLabel = new QLabel(this);
-    //m_logLabel->setFrameShape(QFrame::Box);
     m_logLabel->show();
 
     m_viewTimer = new QTimer(this);
     m_viewTimer->setInterval(20);
 
     connect(m_foreBtn, &QPushButton::pressed, this, &ControlWidget::slot_gofore);
-    connect(m_foreBtn, &QPushButton::released, this, &ControlWidget::slot_stop);
     connect(m_backBtn, &QPushButton::pressed, this, &ControlWidget::slot_goback);
-    connect(m_backBtn, &QPushButton::released, this, &ControlWidget::slot_stop);
     connect(m_leftBtn, &QPushButton::pressed, this, &ControlWidget::slot_turnleft);
-    connect(m_leftBtn, &QPushButton::released, this, &ControlWidget::slot_stop);
     connect(m_rightBtn, &QPushButton::pressed, this, &ControlWidget::slot_turnright);
-    connect(m_rightBtn, &QPushButton::released, this, &ControlWidget::slot_stop);
+    connect(m_drctBtnGroup, static_cast<void(QButtonGroup::*)(QAbstractButton*)>(&QButtonGroup::buttonReleased), 
+        this, &ControlWidget::slot_stop);
     connect(m_viewSlider, &QSlider::valueChanged, this, &ControlWidget::slot_viewChanged);
-    connect(m_viewSlider, &QSlider::sliderReleased, this, &ControlWidget::slot_viewReset);
+    connect(m_resetViewBtn, &QPushButton::clicked, this, &ControlWidget::slot_viewReset);
     connect(m_settingBtn, &QPushButton::clicked, this, &ControlWidget::slot_openSetting);
 
     connect(m_viewTimer, &QTimer::timeout, this, &ControlWidget::slot_viewTimer);
@@ -92,6 +110,7 @@ void ControlWidget::setupUi()
     m_leftBtn->setFocusPolicy(Qt::NoFocus);
     m_rightBtn->setFocusPolicy(Qt::NoFocus);
     m_viewSlider->setFocusPolicy(Qt::NoFocus);
+    m_settingBtn->setFocusPolicy(Qt::NoFocus);
 
     m_picManager = new QNetworkAccessManager(this);
     connect(m_picManager, &QNetworkAccessManager::finished, this, &ControlWidget::slot_showPic);
@@ -101,7 +120,6 @@ void ControlWidget::setupUi()
 
 void ControlWidget::setVideoUrl(QString url)
 {
-    //url = "http://192.168.2.199:8080/?action=snapshot";
     m_picRequest = QNetworkRequest(url);
     m_picManager->get(m_picRequest);
 }
@@ -142,7 +160,7 @@ void ControlWidget::repaintCtl()
     int w = width();
     int h = height();
 
-    int lth = 30;// The width and height of Btn
+    int lth = w / 14;// The width and height of Btn
     int mng = 30;
     int space = 10;// The margin between btns
 
@@ -152,46 +170,49 @@ void ControlWidget::repaintCtl()
     x = mng;
     y = h - (mng + lth + space + lth);
     m_leftBtn->resize(lth, lth);
-    pixmap = QPixmap(":/image/turnleft.png").scaled(m_leftBtn->size());
-    m_leftBtn->setIcon(pixmap);
+    m_leftBtn->setIconSize(m_leftBtn->size());
     m_leftBtn->move(x, y);
 
     x = mng + lth + space;
     y = h - (mng + lth);
     m_backBtn->resize(lth, lth);
-    pixmap = QPixmap(":/image/goback.png").scaled(m_backBtn->size());
-    m_backBtn->setIcon(pixmap);
+    m_backBtn->setIconSize(m_backBtn->size());
     m_backBtn->move(x, y);
 
     x = mng + lth + space + lth + space;
     y = h - (mng + lth + space + lth);
     m_rightBtn->resize(lth, lth);
-    pixmap = QPixmap(":/image/turnright.png").scaled(m_rightBtn->size());
-    m_rightBtn->setIcon(pixmap);
+    m_rightBtn->setIconSize(m_rightBtn->size());
     m_rightBtn->move(x, y);
 
     x = mng + lth + space;
     y = h - (mng + lth + space + lth + space + lth);
     m_foreBtn->resize(lth, lth);
-    pixmap = QPixmap(":/image/gofore.png").scaled(m_foreBtn->size());
-    m_foreBtn->setIcon(pixmap);
+    m_foreBtn->setIconSize(m_foreBtn->size());
     m_foreBtn->move(x, y);
 
-    m_settingBtn->resize(lth, lth);
-    pixmap = QPixmap(":/image/setting.png").scaled(m_settingBtn->size());
-    m_settingBtn->setIcon(pixmap);
+    m_settingBtn->resize(20, 20);
+    m_settingBtn->setIconSize(m_settingBtn->size());
     m_settingBtn->move(10, 10);
 
-    m_infoLabel->resize(100, 100);
+    m_infoLabel->resize(100, 50);
     m_infoLabel->move(w - (100 + 10), 10);
 
     m_logLabel->resize(100, 50);
     m_logLabel->move(10, 60);
 
     m_viewSlider->resize(200, 10);
-    x = w - 200 - mng;
-    y = h - mng - 10;
+    x = w - mng - 200;
+    y = h - mng - 10 - 10;
     m_viewSlider->move(x, y);
+
+    lth = 20;
+    m_resetViewBtn->resize(lth, lth);
+    x = w - mng - 100 - lth / 2;
+    y = h - mng - 10 - 10 - lth;
+    m_resetViewBtn->move(x, y);
+    if (m_viewSlider->value() == 90)
+        m_resetViewBtn->hide();
 }
 
 void ControlWidget::paintEvent(QPaintEvent *event)
@@ -216,23 +237,23 @@ void ControlWidget::keyPressEvent(QKeyEvent *event)
     QWidget::keyPressEvent(event);
     if (event->key() == Qt::Key_W || event->key() == Qt::Key_Up)
     {
-        slot_gofore();
         m_foreBtn->setChecked(true);
+        slot_gofore();
     }
     else if (event->key() == Qt::Key_A || event->key() == Qt::Key_Left)
     {
-        slot_turnleft();
         m_leftBtn->setChecked(true);
+        slot_turnleft();
     }
     else if (event->key() == Qt::Key_S || event->key() == Qt::Key_Down)
     {
-        slot_goback();
         m_backBtn->setChecked(true);
+        slot_goback();
     }
     else if (event->key() == Qt::Key_D || event->key() == Qt::Key_Right)
     {
-        slot_turnright();
         m_rightBtn->setChecked(true);
+        slot_turnright();
     }
 
     if (event->key() == Qt::Key_Q)
@@ -257,11 +278,18 @@ void ControlWidget::keyReleaseEvent(QKeyEvent *event)
         return;
     QWidget::keyReleaseEvent(event);
     if (event->key() == Qt::Key_W || event->key() == Qt::Key_A || event->key() == Qt::Key_S || event->key() == Qt::Key_D)
+    {
+        if (m_drctBtnGroup->checkedButton() != NULL)
+        {
+            m_drctBtnGroup->checkedButton()->setChecked(false);
+        }
+            
         slot_stop();
+    }
     else if (event->key() == Qt::Key_Q || event->key() == Qt::Key_E)
     {
+        m_ctrlaction &= 0x0F;
         m_viewTimer->stop();
-        m_viewSlider->setValue(90);
     }
 }
 
@@ -308,18 +336,12 @@ void ControlWidget::slot_stop()
     emit signal_logmsg("Stop");
 }
 
-void ControlWidget::slot_viewleft()
-{
-    emit signal_logmsg("View left");
-}
-
-void ControlWidget::slot_viewright()
-{
-    emit signal_logmsg("View right");
-}
-
 void ControlWidget::slot_viewChanged(int angle)
 {
+    if (angle == 90)
+        m_resetViewBtn->hide();
+    else
+        m_resetViewBtn->show();
     emit signal_logmsg(QString("View %1").arg(angle));
 }
 
@@ -365,8 +387,11 @@ void ControlWidget::slot_showLog(QString msg)
 void ControlWidget::slot_openSetting()
 {
     ConfigWidget config(this);
-    config.setControlUrl(m_controlUrl);
-    config.setVideoUrl(m_videoUrl);
+    QString url = QString::null;
+    url = m_controlUrl.length() > 0 ? m_controlUrl : "http://192.168.2.199:8081";
+    config.setControlUrl(url);
+    url = m_videoUrl.length() > 0 ? m_videoUrl : "http://192.168.2.199:8080/?action=snapshot";
+    config.setVideoUrl(url);
     config.setShowInfo(m_showInfo);
     config.setShowLog(m_showLog);
     if (config.exec() == QDialog::Accepted)
